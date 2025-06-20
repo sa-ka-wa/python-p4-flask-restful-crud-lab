b8dd320a77c458e8a9e6bbf462c9e871df2a0aa2
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-06-20 15:18:52 +0300

    added crud

--- server/app.py
+++ server/app.py
@@ -16,6 +16,22 @@ db.init_app(app)
 
 api = Api(app)
 
+class Home(Resource):
+
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
 
 class Plants(Resource):
 
@@ -46,6 +62,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
